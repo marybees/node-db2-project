0ea84ebcb2d3cca95ed1d30fcaa5909f98a449b0
Mary Baker <meb601@gmail.com>
2020-10-06 18:31:52 -0400

    Install dependencies, setup project, API test success

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require("express");
+const helmet = require("helmet");
+
+// const db = require("./database/connection");
+
+const server = express();
+
+server.use(helmet());
+server.use(express.json());
+
+const PORT = process.env.PORT || 5000;
+
+server.get("/", (req, res) => {
+  res.status(200).json({ hello: "and welcome" });
+});
+
+server.listen(PORT, () => console.log(`API running on ${PORT}`));
